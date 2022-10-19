1e49736f2fc42e96731c8b6fea80a0039ba6e2bc
csmi0037 <111551414+csmi0037@users.noreply.github.com>
2022-10-19 22:15:09 +1100

    Add files via upload

--- meteoriteinfo.js
+++ meteoriteinfo.js
@@ -6,7 +6,7 @@ function Stony(){
     stony.style.background = "#4eb1e8"
     stonyiron.style.background = "#5A5A5A"
     iron.style.background = "#5A5A5A"
-    let display = `<div style="padding:5%;"><img src="images/chondrule.jpg" width="100%" height="100%"><p class="infotext">Spherical chondrules in a meteorite</p></div>`
+    let display = `<div style="padding:5%;"><img src="images/chondrule.jpg" style="margin-left: 10%" width="80%" height="80%"><p class="infotext">Spherical chondrules in a meteorite</p></div>`
     display += `<b class="infoheading">Chondrite vs Anchondrite:</b>`
     display += `<p class="infotext">While these are both sub-types of stony meteorites, the main difference is the inclusion on chondrules as pictured above.<br><br>Chondrites include chondrules, which are tiny spherical silicate inclusions whereas achondrites do not. </p>`
     display += `<button id="chondrite" onclick="chondrite()" class="buttonselect2">Chondrite</button>`
@@ -20,7 +20,7 @@ function StonyIron(){
     stony.style.background = "#5A5A5A"
     stonyiron.style.background = "#4eb1e8"
     iron.style.background = "#5A5A5A"
-    let display = `<div style="padding:5%;"><img src="images/Esquel.jpg" width="100%" height="100%"><p class="infotext">A slice of Esqual pallasite</p></div>`
+    let display = `<div style="padding:5%;"><img src="images/Esquel.jpg" style="margin-left: 10%" width="80%" height="80%"><p class="infotext">A slice of Esqual pallasite</p></div>`
     display += `<b class="infoheading">Pallasites:</b>`
     display += `<p class="infotext">Usually comprised of olivine crystals in an iron-nickel matrix, allowing beautiful slices to be carved from these as seen above.</p>`
     display += `<b class="infoheading">Mesosiderite:</b>`
@@ -32,7 +32,7 @@ function Iron(){
     stony.style.background = "#5A5A5A"
     stonyiron.style.background = "#5A5A5A"
     iron.style.background = "#4eb1e8"
-    let display = `<div style="padding:5%;"><img src="images/TamentitMeteorite.png" width="100%" height="100%"><p class="infotext">500kg Tamentit Iron Meteorite</p></div>`
+    let display = `<div style="padding:5%;"><img src="images/TamentitMeteorite.png" style="margin-left: 10%" width="80%" height="80%"><p class="infotext">500kg Tamentit Iron Meteorite</p></div>`
     display += `<b class="infoheading">Magmatic Iron:</b>`
     display += `<p class="infotext">Mostly comprised of solid nickel-iron metal and categorised by their magnetic nauture. <br><br>These are usually the biggest meteorites due to the strength of the metal causing them not to break up while travelling through our atmosphere.</p>`
     display += `<b class="infoheading">Non-magmatic or Primative Iron:</b>`
@@ -41,18 +41,18 @@ function Iron(){
 }
 
 function chondrite(){
-    let display = `<div style="padding:5%;"><img src="images/StonyMeteorite.jpg" width="100%" height="100%"><b class="infoheading">Chondrites:</b></div>`
+    let display = `<div style="padding:5%;"><img src="images/StonyMeteorite.jpg" style="margin-left: 10%" width="80%" height="80%"><b class="infoheading">Chondrites:</b></div>`
     display += `<b class="infoheading">Carbonaceous Chondrite:</b>`
-    display += `<p class="infotext">These are categorised by their high proportion of carbon in the form of graphite or carbonates. <br><br>The high carbon prescence is due to a lack of exposure to high temperatures resulting in a hardly chaning from thermal processes.</p>`
+    display += `<p class="infotext">The high carbon prescence is due to a lack of exposure to high temperatures resulting in a hardly chaning from thermal processes.</p>`
     display += `<b class="infoheading">Ordinary Chondrite:</b>`
-    display += `<p class="infotext">The most common type of metorite, comprising of over 85% of all recorded landings. <br><br>While indicating that these are from the most common asteroid, it is instead thought that the type of asteroids these meteorites originate from are much more suited at sending fragments to reach the surface of earth.</p>`
+    display += `<p class="infotext">The most common type of metorite, comprising of over 85% of all recorded landings. This is due to the asteroids these meteorites originate from are much more suited at sending fragments to reach the surface of earth.</p>`
     display += `<b class="infoheading">Enstatite Chondrite:</b>`
     display += `<p class="infotext">A rare type of Chondrite which is rich in the mineral enstatite</p>`
     infodiv.innerHTML = display
 }
 
 function achondrite(){
-    let display = `<div style="padding:5%;"><img src="images/achondrite4.png" width="100%" height="100%"><b class="infoheading">Achondrites:</b></div>`
+    let display = `<div style="padding:5%;"><img src="images/achondrite4.png" style="margin-left: 10%" width="80%" height="80%"><b class="infoheading">Achondrites:</b></div>`
     display += `<b class="infoheading">Asteroidal Achondrite:</b>`
     display += `<p class="infotext">These meteorites are classifyied by originating from an asteroid but contain a different mineral and chemical composition due to melting or crystalisation processes.<br><br> Just like all achondrites, these meteorites do not contain chondrules and instead are representative of basalt or plutonic earth rocks. </p>`
     display += `<b class="infoheading">Lunar:</b>`
